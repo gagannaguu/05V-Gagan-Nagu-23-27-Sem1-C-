a6a61cff7f8d54b5802967fc56fd21d037181e65
Garvit Bhartia <garvit@Garvits-MacBook-Air.local>
2023-09-30 18:43:31 +0530

    sum of digits

---
+++ program34.cpp
@@ -0,0 +1,16 @@
+//Calculate the sum of digits of a number using a do-while loop.
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+int n,sum=0,m;    
+cout<<"Enter a number: ";    
+cin>>n;    
+do{m=n%10;    
+sum=sum+m;    
+n=n/10; 
+} 
+while(n>0);      
+cout<<"Sum is = "<<sum<<endl;    
+return 0;  
+}  
