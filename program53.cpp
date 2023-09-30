b1a6414345495daf76d9b56ede3afefce9eb0380
Gagannagu <142592216+Gagannagu@users.noreply.github.com>
2023-09-30 11:11:32 +0530

    palindrome checker ,program no53

---
+++ program53.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+string str;
+
+cout << "Enter a string: ";
+cin >> str;
+
+string reversed = str;
+reverse(reversed.begin(), reversed.end());
+
+if (str == reversed) {
+cout << "\"" << str << "\" is a palindrome." <<endl;
+} else {
+cout << "\"" << str << "\" is not a palindrome." <<endl;
+}
+
+return 0;
+}
