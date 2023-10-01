0b7a413a268bb8b2ebfa0642b1076e75c0db2629
Gagannagu <142592216+Gagannagu@users.noreply.github.com>
2023-10-01 10:42:59 +0530

    pratterns ,program no91

---
+++ program91.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n;
+    cout << "Enter the number of lines: ";
+    cin >> n;
+
+    cout << endl << "The pattern with " << n << " rows is" << endl << endl ;
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+        for (int k = 0; k < 2*(n-i) - 1; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
