f2e0a1ade4cad089c649b1ceda65205c37b45a0a
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2023-10-01 14:42:11 +0530

    Update program61.cpp

--- program61.cpp
+++ program61.cpp
@@ -22,6 +22,11 @@ int main()
     for (int i = 0; i < size; i++)
     {
         cin >> arr[i];
+        if (arr[i]==0)
+        {
+            cout << "Invalid element. Please enter a positive integer." << endl;
+            return 1; //Exit with an error code
+        }
     }
 
     // Display the elements of the array
