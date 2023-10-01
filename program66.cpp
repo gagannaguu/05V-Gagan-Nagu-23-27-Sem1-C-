41452c8d32046b03f9d4ba0731fc4fac3b6f364a
Garvit Bhartia <garvit@Garvits-MacBook-Air.local>
2023-10-01 17:07:42 +0530

    largest

--- program66.cpp
+++ program66.cpp
@@ -1,32 +1,4 @@
 //Create a program to find the largest element in each row of a matrix.
-// #include<iostream>
-// using namespace std;
-//  const int N = 4;
- 
-// void printArray(int result[], int no_of_rows) {
-//     for (int i = 0; i < no_of_rows; i++) {
-//         cout<< result[i]<<"\n";
-//     }
-// }
- 
-// void maxelement(int no_of_rows, int arr[][N]) {
-//     int result[no_of_rows];
-//     for (int i = 0; i < no_of_rows; i++) {
-//         int max = *max_element(arr[i], arr[i]+N);
-//         result[i] = max;
-//     }
-//     printArray(result,no_of_rows);
-// }
- 
-// int main() {
-
-//  int arr[][N] = {{3, 4, 1, 8},
-//              {1, 4, 9, 11},
-//                     {76, 34, 21, 1},
-//                     {2, 1, 4, 5} };
-//    maxelement(4, arr);
-//     return 0;
-// }
 #include<iostream>
 using namespace std;
 
@@ -34,7 +6,7 @@ int main(){
 
     int r, c;
 
-    cout<<"Enter the number od rows and columns :";
+    cout<<"Enter the number of rows and columns :";
     cin>> r >> c;
 
     int a[r][c];
