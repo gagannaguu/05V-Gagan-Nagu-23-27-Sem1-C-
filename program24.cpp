8012e459bbe559aca0a8e4d7f72400c564b7150c
Garvit Bhartia <garvit@Garvits-MacBook-Air.local>
2023-10-01 12:35:55 +0530

    prime or composite

---
+++ program24.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+  int n, i,flag=0;  
+  cout << "Enter the Number to check Prime: ";  
+  cin >> n;    
+  if (n==1 || n==0){
+  cout<<n<<" neither prime nor composite"<<endl;
+  }
+  else {
+  
+  for(i = 2; i <= n/2; i++)  
+  {  
+      if(n % i == 0)  
+      {  
+          cout<<"Number is not Prime."<<endl;  
+          flag=1;  
+          break;  
+      }  
+  } 
+  if (flag==0) {
+      cout << "Number is Prime."<<endl;  
+  }
+}
+  
+  return 0;  
+}
