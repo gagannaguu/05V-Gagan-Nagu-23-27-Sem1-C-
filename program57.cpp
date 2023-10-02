e03211efb07ce9bb9f7e2ab70be1f4775ddb692e
Garvit Bhartia <garvit@Garvits-MacBook-Air.local>
2023-10-02 16:15:42 +0530

    ahdsjdh

--- program57.cpp
+++ program57.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 int main ()
 {
-    int A[10], B[10], n, i, j, k = 0;
+    int j, n,k = 0;
     cout << "Enter size of array : ";
     cin >> n;
+   int A[n], B[n];
     cout << "Enter elements of array : ";
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         cin >> A[i];    
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (j = 0; j < k; j++)
+        for ( j = 0; j < k; j++)
         {
             if (A[i] == B[j])
                 break;
@@ -19,11 +20,12 @@ int main ()
         if (j == k)
         {
             B[k] = A[i];
+            cout<<"annn";
             k++;
         }
     }
     cout << "Repeated elements after deletion : ";
-    for (i = 0; i < k; i++)
+    for (int i = 0; i < k; i++)
         cout << B[i] << " ";
     return 0;
 }
