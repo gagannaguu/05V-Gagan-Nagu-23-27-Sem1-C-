12ac18ae368d69479d7a6fb71fb74b90fc9483e5
Jithendra-Bathala236 <142774501+Jithendra-Bathala236@users.noreply.github.com>
2023-10-02 18:48:46 +0530

    Update program41.cpp

--- program41.cpp
+++ program41.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 int main() {
-int num1, num2, max;
-
-
-cout << "Enter two numbers: ";
-cin >> num1 >> num2;
-
-
-max = (num1 > num2) ? num1 : num2;
-
-
-while (true) {
-if (max % num1 == 0 && max % num2 == 0) {
-cout << "LCM of " << num1 << " and " << num2 << " is " << max <<endl;
-break;
-}
-max++;
-}
+  int num1, num2, max;
+  
+  
+  cout << "Enter two numbers: ";
+  cin >> num1 >> num2;
+  
+  
+  max = (num1 > num2) ? num1 : num2;
+  
+  
+  while (true) {
+    if (max % num1 == 0 && max % num2 == 0) {
+      cout << "LCM of " << num1 << " and " << num2 << " is " << max <<endl;
+      break;
+    }
+    max++;
+  }
 
 return 0;
 }
