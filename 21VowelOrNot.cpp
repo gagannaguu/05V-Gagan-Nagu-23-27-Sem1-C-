7736737b8653562ea2e68a1550338e06ec495be5
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2023-10-02 23:37:34 +0530

    Update and rename program21.cpp to 21VowelOrNot.cpp

--- 21VowelOrNot.cpp
+++ 21VowelOrNot.cpp
@@ -6,6 +6,7 @@ int main()
 char c;
 cout<<"Enter a character ";
 cin>>c;
+//checks if the user given letter is a vowel(a,e,i,o,u,A,E,I,O,U) or not
 if (c =='a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c=='O'||c=='u'||c=='U')
 {
     cout<<c<< " is a vowel";
