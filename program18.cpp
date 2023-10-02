a50f9fa3a72570879544c6384ea5a1cd7396067c
Gagannagu <142592216+Gagannagu@users.noreply.github.com>
2023-10-02 12:54:57 +0530

    Even or odd ,program no18

---
+++ program18.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int input;
+
+cout<<"Enter a number: "<<endl;
+cin>>input;
+
+if ( input %2 == 0){
+  cout <<"The given number is even!"<<endl;
+
+}
+else {
+  cout <<"The given number is odd!"<<endl;
+}
+
+
+
+
+return 0;
+
+}
