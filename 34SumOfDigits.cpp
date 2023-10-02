3321c864aa9fa4ed51423b5d63dfac4689f170c4
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2023-10-02 23:44:45 +0530

    Update and rename program34.cpp to 34SumOfDigits.cpp

--- 34SumOfDigits.cpp
+++ 34SumOfDigits.cpp
@@ -7,10 +7,12 @@ int n,sum=0,m;
 cout<<"Enter a number: ";    
 cin>>n;  
 //using do while loop  
+//taking out last digit of the number and adding it to the sum variable and updating value of n without the last digit
 do{m=n%10;    
 sum=sum+m;    
 n=n/10; 
 } 
+//doing this loop till n is not empty
 while(n>0);      
 cout<<"Sum is = "<<sum<<endl;    
 return 0;  
