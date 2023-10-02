337aed896492e934e5421ace9be4a33fcc27c64e
atharavpatil77 <2023.atharavp@isu.ac.in>
2023-10-02 13:52:07 +0530

    programs uploaded

---
+++ pro16.cpp
@@ -0,0 +1,21 @@
+//program to find the length of string without using length function
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+   int i=0;
+   int count=0;
+   string characters; 
+
+   cout<<"\nEnter any string :: ";
+   getline (cin,characters) ; // using getline function to include all types of input
+
+   for(i=0; characters[i]; i++)
+   {
+        count++;
+   }
+   cout<<"\nLength of String [ "<<characters<<" ] is :: "<<count<<"\n";
+}
