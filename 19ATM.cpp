6e89ccce08b474318f9aa53ef71ba8d6b4183e0c
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2023-10-02 23:36:00 +0530

    Update and rename program19.cpp to 19ATM.cpp

--- 19ATM.cpp
+++ 19ATM.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 int main() {
+    //balance is 50000 Rs
     float balance = 50000.0;
     int choice;
     float amount;
 
+    //to print infinitely
     while (true) {
         cout << "\t\t XYZ ATM \t\t" << endl;
         cout << "*****************" << endl;
@@ -16,11 +18,14 @@ int main() {
         cout << "Enter your choice: ";
         cin >> choice;
 
+        //using switch loop to print a menu and let user choose which block of code they want to access
+        //choice 1 to see account balance
         switch (choice) {
             case 1:
                 cout << "Your balance is: ₹" << balance << "\n"<<endl;
                 break;
 
+            //choice 2 to deposit money (only if amount to deposit is >0)
             case 2:
                 cout << "Enter the amount to deposit: ₹";
                 cin >> amount;
@@ -32,9 +37,11 @@ int main() {
                 }
                 break;
 
+            //choice 3 to withdraw money
             case 3:
                 cout << "Enter the amount to withdraw: ₹";
                 cin >> amount;
+                //only allowed to withdraw if amount to be withdrawn <= account balance and is a positive number
                 if (amount > 0 && amount <= balance) {
                     balance -= amount;
                     cout << "Withdrawn ₹" << amount << " successfully.\n" << endl;
@@ -45,10 +52,12 @@ int main() {
                 }
                 break;
 
+            //choice 4 to exit the ATM
             case 4:
                 cout << "Exiting the ATM. Have a nice day!" << endl;
                 return 0;//ends entire main function's execution
 
+            //default message to print if user enters a value other than 1,2,3,4
             default:
                 cout << "Invalid choice. Please select a valid option.\n" << endl;
                 break;
