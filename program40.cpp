ee70ec024072809a812892871ee8bff8731bcaf7
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2023-10-02 16:27:49 +0530

    factorial using nested loop

--- program40.cpp
+++ program40.cpp
@@ -7,9 +7,13 @@ int main()
     cout << "Enter a number to find it's factorial: ";
     cin >> n;
     // check if number is greater than zero(as only positive numbers have factorial values)
-    if (n <= 0)
+    if (n < 0)
     {
         cout << "Invalid Number!" << endl;
+    }
+    else if (n==0)
+    {
+        cout << "Factorial of 0 is 1" << endl;
     }
     else
     {
